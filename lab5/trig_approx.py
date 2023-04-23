ada331f87d017f5a3b7408bc1947922dc7c9bea4
Errno-404 <adamkmezydlo@gmail.com>
2023-04-23 22:57:38 +0200

    fixed trig_approx.py

--- lab5/trig_approx.py
+++ lab5/trig_approx.py
@@ -13,11 +13,10 @@ def func(x, k=3, m=3):
     return np.exp(-k * np.sin(m * x)) + k * np.sin(m * x) - 1
 
 
-def draw(x, y, n, m, f):
+def draw(x, y, n, m, xaxis, yaxis):
     plt.scatter(x, y, label="data", color="red")
-    xaxis = np.linspace(A, B, POINTS)
     plt.plot(xaxis, func(xaxis), label="F(x)", color="red")
-    plt.plot(xaxis, f(xaxis), label="f(x)", color="blue")
+    plt.plot(xaxis, yaxis, label="f(x)", color="blue")
     plt.title(f"Aproksymacja wielomianami trygonometrycznymi dla n = {n} i  m = {m}")
     plt.xlim(A, B)
     xticks = np.arange(-3 * np.pi / 2, 5 * np.pi / 2 + 1e-9, np.pi / 2)  # dodajemy 1e-9, aby dodać 5π/2
@@ -31,74 +30,12 @@ def draw(x, y, n, m, f):
     plt.show()
 
 
-class TrigonometricApproximation:
-    def __init__(self, X, Y, n, m, start, stop):
-        if m > np.floor((n - 1) / 2):
-            raise Exception("m cannot be greater than floor of (n-1)/2")
-        self.X = X
-        self.Y = Y
-        self.n = n
-        self.m = m
-        self.start = start
-        self.stop = stop
-        self.A = np.zeros(self.n)
-        self.B = np.zeros(self.n)
-        self.scale_to_2pi()
-        self.compute_A_and_B()
-        self.scale_from_2pi()
-
-    def scale_to_2pi(self):
-        range_length = self.stop - self.start
-        for i in range(len(self.X)):
-            self.X[i] /= range_length
-            self.X[i] *= 2 * np.pi
-            self.X[i] += -np.pi - (2 * np.pi * self.start / range_length)
-
-        # print(self.X)
-
-    # scale_to_2pi to prepare X
-    # compute_A_and_B
-
-    def compute_A_and_B(self):
-        for i in range(self.n):
-            ai = sum(self.Y[j] * np.cos(i * self.X[j]) for j in range(self.n))
-            bi = sum(self.Y[j] * np.sin(i * self.X[j]) for j in range(self.n))
-            self.A[i] = 2 * ai / self.n
-            self.B[i] = 2 * bi / self.n
-
-        print(self.A, self.B)
-
-    def scale_from_2pi(self):
-        range_length = self.stop - self.start
-        for i in range(len(self.X)):
-            self.X[i] -= -np.pi - (2 * np.pi * self.start / range_length)
-            self.X[i] /= 2 * np.pi
-            self.X[i] *= range_length
-
-    def scale_point_to_2pi(self, x):
-        range_length = self.stop - self.start
-        x /= range_length
-        x *= 2 * np.pi
-        x += -np.pi - (2 * np.pi * self.start / range_length)
-        return x
-
-    def approximate(self, X):
-        points = []
-        for x in X:
-            cp_x = deepcopy(x)
-            cp_x = self.scale_point_to_2pi(cp_x)
-            approximated_x = 1 / 2 * self.A[0] + sum(self.A[j] * np.cos(j * cp_x) + self.B[j] * np.sin(j * cp_x)
-                                                     for j in range(1, self.m + 1))
-            points.append(approximated_x)
-        return self.A, self.B
-
-
 def scale_to_2pi(X):
     x_prim = deepcopy(X)
     n = len(x_prim)
     range_length = B - A
     for i in range(n):
-        x_prim[i] = X[i] / range_length
+        x_prim[i] = x_prim[i] / range_length
         x_prim[i] = x_prim[i] * 2 * np.pi
         x_prim[i] = x_prim[i] + -np.pi - (2 * np.pi * A / range_length)
 
@@ -142,38 +79,22 @@ def approximate(X, A, B, m):
 def calculate(n, m):
     X = np.linspace(A, B, n)
     Y = func(X)
-
-
-
     scaled_x = scale_to_2pi(X)
     a, b = compute_A_and_B(scaled_x, Y, n)
-    print(a, b)
+    c = X
+    print(approximate(c, a, b, m))
 
+    xaxis = np.linspace(A, B, POINTS)
+    yaxis = approximate(xaxis, a, b, m)
 
-
-
-
-    # yaxis = approximate(scaled_x, a, b, m)
-    # xaxis = np.linspace(A, B, len(yaxis))
-
-
-
-
-
-
-    trigonometric_approximation = TrigonometricApproximation(X, Y, n, m, A, B)
-    a_p, b_p = trigonometric_approximation.approximate(X)
-    for i in range(len(a)):
-        if a[i] != a_p[i]:
-            print(abs(a[i] - a_p[i]))
-    # draw(X, Y, n, m, trigonometric_approximation.approximate)
+    draw(X, Y, n, m, xaxis, yaxis)
 
 
 def main():
     if not os.path.exists(IMG_PATH):
         os.makedirs(IMG_PATH)
 
-    calculate(100, 23)
+    calculate(50, 12)
 
 
 if __name__ == "__main__":
