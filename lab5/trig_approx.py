8f6ccd2a237bf4c56b0a588d0798b682e0feaf08
Errno-404 <adamkmezydlo@gmail.com>
2023-04-23 23:31:48 +0200

    finished trig_approx.py I guess

--- lab5/trig_approx.py
+++ lab5/trig_approx.py
@@ -2,8 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from copy import deepcopy
+import csv
 
 IMG_PATH = "./img"
+ERROR_PATH = "./data"
 A = -np.pi
 B = 2 * np.pi
 POINTS = 1000
@@ -13,56 +15,22 @@ def func(x, k=3, m=3):
     return np.exp(-k * np.sin(m * x)) + k * np.sin(m * x) - 1
 
 
-def draw(x, y, n, m, xaxis, yaxis):
-    plt.scatter(x, y, label="data", color="red")
-    plt.plot(xaxis, func(xaxis), label="F(x)", color="red")
-    plt.plot(xaxis, yaxis, label="f(x)", color="blue")
-    plt.title(f"Aproksymacja wielomianami trygonometrycznymi dla n = {n} i  m = {m}")
-    plt.xlim(A, B)
-    xticks = np.arange(-3 * np.pi / 2, 5 * np.pi / 2 + 1e-9, np.pi / 2)  # dodajemy 1e-9, aby dodać 5π/2
-    xticklabels = ['-3π/2', '-π', '-π/2', '0', 'π/2', 'π', '3π/2', '2π', '5π/2']
-    plt.xticks(xticks, xticklabels)
-
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.legend(loc='best')
-    plt.savefig(IMG_PATH + f"/img_{n}_{m}")
-    plt.show()
-
-
 def scale_to_2pi(X):
     x_prim = deepcopy(X)
-    n = len(x_prim)
     range_length = B - A
-    for i in range(n):
-        x_prim[i] = x_prim[i] / range_length
-        x_prim[i] = x_prim[i] * 2 * np.pi
-        x_prim[i] = x_prim[i] + -np.pi - (2 * np.pi * A / range_length)
-
-    # print(x_prim)
+    x_prim = (x_prim - A) / range_length * 2 * np.pi - np.pi
     return x_prim
-
-    # scale_to_2pi to prepare X
-    # compute_A_and_B
-
-
-def compute_A_and_B(X, Y, n):
-    left = np.zeros(n)
-    right = np.zeros(n)
-    for i in range(n):
-        ai = sum(Y[j] * np.cos(i * X[j]) for j in range(n))
-        bi = sum(Y[j] * np.sin(i * X[j]) for j in range(n))
-        left[i] = 2 * ai / n
-        right[i] = 2 * bi / n
-    return left, right
 
 
 def scale_point_to_2pi(x):
     range_length = B - A
-    x /= range_length
-    x *= 2 * np.pi
-    x += -np.pi - (2 * np.pi * A / range_length)
-    return x
+    return (x - A) / range_length * 2 * np.pi - np.pi
+
+
+def compute_A_and_B(X, Y, n):
+    left = 2 * np.array([sum(Y[j] * np.cos(i * X[j]) for j in range(n)) / n for i in range(n)])
+    right = 2 * np.array([sum(Y[j] * np.sin(i * X[j]) for j in range(n)) / n for i in range(n)])
+    return left, right
 
 
 def approximate(X, a, b, m):
@@ -80,20 +48,53 @@ def calculate(n, m):
     x = np.linspace(A, B, n)
     y = func(x)
 
-    scaled_x = scale_to_2pi(x)
-    a, b = compute_A_and_B(scaled_x, y, n)
-
+    a, b = compute_A_and_B(scale_to_2pi(x), y, n)
     xaxis = np.linspace(A, B, POINTS)
     yaxis = approximate(xaxis, a, b, m)
 
     draw(x, y, n, m, xaxis, yaxis)
+    return estimate_error2(func, xaxis, yaxis)
+
+
+def estimate_error2(f, x, y):
+    return np.sqrt(np.sum((f(x) - y) ** 2)) / len(x)
+
+
+def draw(x, y, n, m, xaxis, yaxis):
+    plt.scatter(x, y, label="data", color="red")
+    plt.plot(xaxis, func(xaxis), label="F(x)", color="red")
+    plt.plot(xaxis, yaxis, label="f(x)", color="blue")
+    plt.title(f"Aproksymacja wielomianami trygonometrycznymi dla n = {n} i  m = {m}")
+    plt.xlim(A, B)
+    plt.xticks([-3 * np.pi / 2, -np.pi, -np.pi / 2, 0, np.pi / 2, np.pi, 3 * np.pi / 2, 2 * np.pi, 5 * np.pi / 2],
+               ['-3π/2', '-π', '-π/2', '0', 'π/2', 'π', '3π/2', '2π', '5π/2'])
+
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.legend(loc='best')
+    plt.savefig(IMG_PATH + f"/img_{n}_{m}")
+    plt.show()
 
 
 def main():
     if not os.path.exists(IMG_PATH):
         os.makedirs(IMG_PATH)
 
-    calculate(50, 12)
+    if not os.path.exists(ERROR_PATH):
+        os.makedirs(ERROR_PATH)
+
+    if not os.path.isfile(ERROR_PATH + "/errors.csv"):
+        with open(ERROR_PATH + "/errors.csv", 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["m", "n", "error"])
+
+
+    for n in range(5, 200, 5):
+        for m in range(1, int((n-1) / 2) + 1):
+            # print(n, m)
+            print(calculate(n, m))
+
+    # print(calculate(50, 12))
 
 
 if __name__ == "__main__":
